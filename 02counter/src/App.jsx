ac4bf68346b7646f5b84672bae319f69e2a59883
Abhay Yadav <23abhayyadav32@gmail.com>
2026-05-20 15:21:11 +0530

    counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter]= useState(10)
+
+  // let counter = 10
+
+  const addValue = () => {
+    // console.log("value added", counter);
+    // counter = counter + 1
+    setCounter(counter+1)
+    
+  }
+
+  const removeValue = () => {
+    setCounter(counter -1)
+  }
+
+  return (
+    <>
+    <h1>Hello React</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Add value</button>
+    <br />
+    <br />
+    <button
+    onClick={removeValue}
+    >Remove value</button>
+    </>
+  )
+}
+
+export default App
