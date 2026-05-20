25d7d9a97a06f4000bcea8d7eee40f4b54692c3d
Abhay Yadav <23abhayyadav32@gmail.com>
2026-05-20 15:06:46 +0530

    Vitereact

---
+++ 01vitereact/src/1test.jsx
@@ -0,0 +1,7 @@
+function Test (){
+    return(
+        <h1>React Test</h1>
+    )
+}
+
+export default Test
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+ import Test from "./1test"
+function App() {
+
+  return (
+    <>
+      <Test/>
+      <h2>Learn from Chai aur code</h2>
+      <p>PF Youtube</p>
+
+    </>
+  )
+}
+
+export default App
